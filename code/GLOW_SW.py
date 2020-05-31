28d4f72e2bddc668b0c43c56aba757412bca7fe1
Steven Walton <swalton2@cs.uoregon.edu>
2020-05-31 00:54:40 -0700

    I think I have everything working except for the split. How to deal with the size decreasing?

--- code/GLOW_SW.py
+++ code/GLOW_SW.py
@@ -29,6 +29,63 @@ data = DataLoader(mnist_data,
                   num_workers=numworkers,
                   )
 
+def squeeze(x,f=2):
+    b,c,h,w = x.size()
+    assert(h%f==0 and w%f == 0),f"Squeeze Error: h%f = {h%f} and w%f = {w%f}"
+    x = x.reshape(-1,c,h//f,f,w//f,f)
+    x = x.permute(0,1,3,5,2,4)
+    #x = x.reshape(-1,int(h/f),int(w/f),c*f*f)
+    x = x.reshape(-1,c*f*f, int(h/f),int(w/f))
+    return x
+
+def unsqueeze(x,f=2):
+    b,c,h,w = x.size()
+    x = x.reshape(-1,int(c/f/f),f,f,h,w)
+    x = x.permute(0,1,4,2,5,3)
+    #x = x.reshape(-1,h*f,w*f,int(c/f)**2)
+    x = x.reshape(-1,int(c/f/f),h*f,w*f)
+    return x
+
+#def split(x):
+#    '''
+#    Split along channel direction
+#    '''
+#    b,c,w,h = x.size()
+#    x0 = x[:,c//2:,:,:]
+#    x1 = x[:,:c//2,:,:]
+#    return x0,x1
+
+class Split(nn.Module):
+    def __init__(self, c):
+        super(Split,self).__init__()
+        self.mean = nn.Conv2d(c//2, c//2, kernel_size=3, padding=1)
+        self.log_std = nn.Conv2d(c//2, c//2, kernel_size=3, padding=1)
+
+        self.mean.weight.data.zero_()
+        self.mean.bias.data.zero_()
+        self.log_std.weight.zero_()
+        self.log_std.bias.zero_()
+
+    def forward(self,x):
+        x0, x1 = x.chunk(2, dim=1)
+        mu = self.mean(x0)
+        std = self.log_std(x1)
+        z = (x1-mu)*torch.exp(-std)
+        logdet = -std.sum([1,2,3])
+        return x0,x1, logdet
+
+    def backward(self,y0, y1):
+        mu = self.mean(y0)
+        std = self.log_std(x0)
+        x1 = y1 * torch.exp(std) - mu
+        legdet = -std.sum([1,2,3])
+        return torch.cat((y0,x1),dim=1), logdet
+
+
+def concat(x0,x1):
+    x = torch.cat((x0,x1),dim=1)
+    return x
+
 class LinearBlock(nn.Module):
     def __init__(self,_in,_out,reg=True,inplace=True):
         super(LinearBlock,self).__init__()
@@ -51,12 +108,22 @@ class Actnorm(nn.Module):
         self.b = nn.Parameter(torch.zeros(dims))
 
     def forward(self,x):
-        y = torch.add(torch.mul(self.s,x), self.b)
-        logdet = torch.mul(torch.mul(h,w), torch.sum(torch.abs(s)))
+        print(x.size())
+        b,c,h,w = x.size()
+        s = self.s.view(-1,c,h,w)
+        b = self.b.view(-1,c,h,w)
+        print(f"actnorm s {s.size()}")
+        print(f"actnorm b {b.size()}")
+        #y = torch.add(torch.mul(s,x), b)
+        y = x*s + b
+        #logdet = torch.mul(torch.mul(h,w), torch.sum(torch.abs(s)))
+        logdet = h*w*torch.log(self.s.abs()).sum()
         return y, logdet
 
     def backward(self,y):
+        b,c,h,w = y.size()
         x = torch.div(torch.sub(y,self.b),self.s)
+        logdet = -h*w*torch.log(self.s.abs()).sum()
         return x
 
 class InvertConv(nn.Module):
@@ -72,28 +139,43 @@ class InvertConv(nn.Module):
 
     def forward(self, x):
         b,c,h,w = x.size()
-        y = F.conv2d(x,self.W.view(self.c,self.c,1,1))
-        logdet = h*w*torch.sum(torch.log(torch.abs(self.s)))
+        #print(f"In invertconv forward x {x.size()}, W {self.W.size()}\
+        #        reshaped {(self.W.view(self.c, self.c,1,1)).size()}")
+        #y = F.conv2d(x,self.W.view(self.c,self.c,1,1))
+        y = F.conv2d(x, self.W.unsqueeze(-1).unsqueeze(-1))
+        #logdet = h*w*torch.sum(torch.log(torch.abs(self.s)))
+        # Don't understand pivots in torch.lu
+        #logdet = h*w*torch.det(torch.abs(self.W))
+        logdet = h*w*torch.slogdet(self.W)[1]
         return y, logdet
 
     def backward(self, y):
-        x = torch.mul(torch.inverse(self.W,y))
+        #x = torch.mul(torch.inverse(self.W,y))
+        b,c,h,w = y.size()
+        Winv = self.W.inverse()
+        x = F.conv2d(y, Winv.unsqueeze(-1).unsqueeze(-1))
+        logdet = -h*w*torch.slogdet(self.W)[1]
         return x
 
 class FlowStep(nn.Module):
     def __init__(self,
                  c,
-                 reverse,
+                 reverse=False,
                  ):
         super(FlowStep,self).__init__()
-        self.an = ActNorm()
-        self.inv = InvertConv(c)
-        self.affine = AffineCoupling(reverse)
+        self.an = Actnorm(28**2)
+        #self.an = Actnorm(512)
+        self.inv = InvertConv(c*4)
+        self.affine = AffineCoupling(reverse=reverse)
 
     def forward(self,x):
+        print(f"FlowStep x {x.size()}")
         x,logdet = self.an.forward(x)
+        print(f"FlowStep after Actnorm {x.size()}")
         x,logdet = self.inv.forward(x)
+        print(f"FlowStep after inv {x.size()}")
         x,logdet = self.affine.forward(x)
+        print(f"FlowStep after affine {x.size()}")
         return x, logdet
 
     def backward(self,y):
@@ -102,29 +184,117 @@ class FlowStep(nn.Module):
         y = self.an.backward(y)
         return y
 
+class GLOW(nn.Module):
+    def __init__(self,
+                 c,
+                 k=3,
+                 L=3
+                 ):
+        super(GLOW, self).__init__()
+        self.k = k
+        self.L = L
+        self.FS = FlowStep(c)
+        self.FSR = FlowStep(c,reverse=True)
+        self.splt = Split(4)
+
+    def LStep(self,x):
+        print(f"Into LStep {x.size()}")
+        x = squeeze(x)
+        print(f"After squeeze {x.size()}")
+        for i in range(self.k):
+            print(f"At step {i} {x.size()}")
+            if i%2 == 0:
+                x, logdet = self.FS(x)
+            else:
+                x, logdet = self.FSR(x)
+        x,z = self.split(x)
+        print(f"After skip {x.size()}")
+        return x,z, logdet
+
+    def LStepBack(self,x0,x1):
+        x = self.split.backward(x0,x1)
+        #x = squeeze(x)
+        for i in range(self.k):
+            if i%2 == 0:
+                x, logdet = self.FS.backward(x)
+            else:
+                x, logdet = self.FSR.backward(x)
+        #x,z = split(x)
+        x = self.unsqueeze(x)
+        return x, lodget#,z
+
+    def forward(self,x):
+        print(f"Glow init {x.size()}")
+        for i in range(self.L-1):
+            print(f"L step {i}")
+            x,z, lodget = self.LStep(x)
+        x = squeeze(x)
+        #for i in range(self.k):
+        #    x,logdet = self.FlowStep(x)
+        return x, logdet
+
+    def backward(self,y):
+        for i in range(self.k):
+            y, lodget = self.FlowStep.backward(y)
+        y = unsqueeze(y)
+        # There needs to be a concat here
+        for i in range(self.L):
+            y, lodget = self.LStepBack(y)
+
+        return y, lodget
+
+class Coupling(nn.Module):
+    def __init__(self,c):
+        super(Coupling, self).__init__()
+        self.c = 4#c
+        self.NN = nn.Sequential(nn.Conv2d(self.c//2,self.c,kernel_size=3),#, padding=1),
+                                nn.ReLU(),
+                                nn.Conv2d(self.c,self.c,kernel_size=1),#, padding=1),
+                                nn.ReLU(),
+                                nn.Conv2d(self.c,self.c,kernel_size=1,padding=1)
+                                )
+        self.NN[4].weight.data.zero_()
+        self.NN[4].bias.data.zero_()
+
+    def forward(self,x):
+        #print(f"Inside coupling {x.size()}")
+        x = self.NN(x)
+        #print(f"After layers {x.size()}")
+        logs = x[:,self.c//2:,:,:]
+        #print(f"got log s {logs.size()}")
+        t = x[:,:self.c//2,:,:]
+        #print(f"got t {t.size()}")
+        return logs, t
 
 class AffineCoupling(nn.Module):
     def __init__(self,
-                 c=512,
+                 c=2,
                  reverse=False,
                 ):
         super(AffineCoupling,self).__init__()
-        self.NN = nn.Sequential(nn.Conv2d(c//2,c,kernel_c=3),
-                                nn.ReLU(),
-                                nn.Conv2d(c,c,kernel_c=1),
-                                nn.ReLU(),
-                                )
+        #self.NN = nn.Sequential(nn.Conv2d(c//2,c,kernel_size=3, padding=1),
+        #                        nn.ReLU(),
+        #                        nn.Conv2d(c,c,kernel_size=1, padding=1),
+        #                        nn.ReLU(),
+        #                        nn.Conv2d(c,c,kernel_size=1,padding=1)
+        #                        )
+        #print(f"NN: {self.NN}")
+        #self.NN[4].weight.data.zero_()
+        #self.NN[4].bias.data.zero_()
         self.reverse = reverse
         W = torch.zeros(c,c)
         self.W = nn.Parameter(W)
         self.c = c
+        self.coup = Coupling(c)
 
     def split(self,x):
-        if len(x.size()) == 4:
-            B,C,H,W = x.size()
-            size = C*H*W
-        else:
-            B,size = x.size()
+        #if len(x.size()) == 4:
+        #    B,C,H,W = x.size()
+        #    size = C*H*W
+        #else:
+        #    B,size = x.size()
+        B,C,H,W = x.size()
+        size = C*H*W
         x = x.reshape(B,-1)
         x1 = x[:,size//2:]
         x1.requires_grad_(True)
@@ -133,16 +303,29 @@ class AffineCoupling(nn.Module):
         return x1,x2
 
     def forward(self,x):
+        print(f"In Affine {x.size()}")
         if not self.reverse:
-            x_a,x_b = self.split(x)
+            #x_a,x_b = self.split(x)
+            x_a,x_b = x.chunk(2,1)
         else:
-            x_b,x_a = self.split(x)
-        o = self.NN(x_b)
-        o = F.conv2d(o, self.W.view(self.c,self.c,1,1))
+            #x_b,x_a = self.split(x)
+            x_b, x_a = x.chunk(2,1)
+        print(f"After split {x_b.size()}")
+        #o = self.NN(x_b)
+        #print(f"After NN {o.size()}")
+        #o = F.conv2d(o, self.W.view(self.c,self.c,1,1))
+        #print(f"After conv2d {o.size()}")
         #log_s, t = self.NN.forward(x_b)
+        #log_s, t = o.chunk(2,1)
+        log_s, t = self.coup(x_b)
         s = torch.exp(log_s)
-        y_a = torch.add(torch.mult(s,x_a),t)
+        #y_a = torch.add(torch.mult(s,x_a),t)
+        print(f"x_a {x_a.size()}")
+        print(f"s {s.size()}")
+        y_a = (x_a*s) + t
+        print(f"y_a {y_a.size()}")
         y_b = x_b
+        print(f"y_b {y_b.size()}")
         if not self.reverse:
             y = torch.cat((y_a,y_b), dim=1)
         else:
@@ -166,112 +349,9 @@ class AffineCoupling(nn.Module):
             x = torch.cat((x_b, x_a), dim=1)
         return x
 
-class Coupling(nn.Module):
-    def __init__(self,dim, numLayers=5):
-        super(Coupling,self).__init__()
-        layers = nn.ModuleList()
-        layers.append( LinearBlock(dim//2,1000))
-        for i in range(numLayers):
-            layers.append(LinearBlock(1000,1000))
-        layers.append(LinearBlock(1000,dim//2,reg=False))
-        self.layers = layers
-
-    def forward(self,x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
-
-class Additive(nn.Module):
-    def __init__(self,dim, reverse=False, numLayers=5):
-        super(Additive,self).__init__()
-        self.numLayers = numLayers
-
-        self.layers = Coupling(dim)
-        self.reverse = reverse
-
-    def split(self,x):
-        if len(x.size()) == 4:
-            B,C,H,W = x.size()
-            size = C*H*W
-        else:
-            B,size = x.size()
-        x = x.reshape(B,-1)
-        x1 = x[:,size//2:]
-        x1.requires_grad_(True)
-        x2 = x[:,:size//2]
-        x2.requires_grad_(True)
-        return x1,x2
-
-    def forward(self,x):
-        if not self.reverse:
-            x1,x2 = self.split(x)
-        else:
-            x2,x1 = self.split(x)
-        y1 = x1
-        y2 = x2 + self.layers(x1)
-        log_det = torch.zeros(x.size()[0])
-        if not self.reverse:
-            o = torch.cat((y2,y1),dim=1)
-        else:
-            o = torch.cat((y1,y2),dim=1)
-        return o,log_det
-
-    def backward(self,x):
-        if not self.reverse:
-            y1,y2 = self.split(x)
-        else:
-            y2,y1 = self.split(x)
-        x1 = y1
-        #x2 = y2 - self.layers.backward(x1)
-        x2 = y2 - self.layers(x1)
-        log_det = torch.zeros(x.size()[0])
-        if not self.reverse:
-            o = torch.cat((x2,x1),dim=1)
-        else:
-            o = torch.cat((x1,x2),dim=1)
-        return o,log_det
-
-class Scale(nn.Module):
-    def __init__(self,dim=28**2):
-        super(Scale,self).__init__()
-        self.scale = nn.Parameter(torch.ones(1,dim,requires_grad=True))
-
-    def forward(self,x):
-        x = x*torch.exp(self.scale)
-        log_det = torch.sum(self.scale,dim=1)
-        return x,log_det
-
-    def backward(self,x):
-        x = x*torch.exp(-self.scale)
-        log_det = torch.sum(-self.scale,dim=1)
-        return x,log_det
-
-
-class Nice(nn.Module):
-    def __init__(self,dim,numLayers=5):
-        super(Nice,self).__init__()
-        self.a = nn.ModuleList([Additive(dim=dim, reverse=(i%2)) for i in range(4)] )
-        self.s = Scale(dim)
-
-    def forward(self,x):
-        for layer in self.a:
-            x,log_det = layer(x)
-        x,log_det = self.s(x)
-        return x,log_det
-
-    def backward(self,x):
-        x,log_prob = self.s.backward(x)
-        for layer in self.a[::-1]:
-            x,_= layer.backward(x)
-        return x,log_prob
-
-
 dim = 28**2
-net = Nice(dim).to(device)
-#net = Additive(28**2).to(device)
-#print(net)
-#prior = torch.distributions.Normal(torch.tensor(0.).to(device),
-#                                   torch.tensor(1.).to(device))
+#net = Nice(dim).to(device)
+net = GLOW(c=1).to(device)
 base_distribution = Uniform(torch.zeros(dim).to(device),
         torch.ones(dim).to(device))
 transforms = [SigmoidTransform().inv, AffineTransform(loc=0, scale=1)]
@@ -279,8 +359,8 @@ logistic = TransformedDistribution(base_distribution, transforms)
 
 gaussian = MultivariateNormal(torch.zeros(dim).to(device),
         torch.eye(dim).to(device))
-#prior = gaussian
-prior = logistic
+prior = gaussian
+#prior = logistic
 lr = 1e-4
 optimizer = optim.Adam(net.parameters(), lr, eps=1e-4)
 epochs = 1500
